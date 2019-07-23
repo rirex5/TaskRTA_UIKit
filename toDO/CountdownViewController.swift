d55642b518f54db6f2b43255c9e0b9628c4830bc
Atsushi Otsubo <m@rirex.net>
2019-07-23 20:55:55 +0900

    [add]UIの配置

--- toDO/CountdownViewController.swift
+++ toDO/CountdownViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CountdonwViewController
 //  toDO
 //
 //  Created by Mercury on 2019/07/23.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CountdonwViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
