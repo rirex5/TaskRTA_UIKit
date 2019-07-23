0cd1fb5afa7cc28baa134389469414b382db9dd9
Atsushi Otsubo <m@rirex.net>
2019-07-23 23:28:06 +0900

    [add] カウントダウンの実装

--- toDO/CountdownViewController.swift
+++ toDO/CountdownViewController.swift
@@ -9,20 +9,56 @@
 import UIKit
 
 class CountdownViewController: UIViewController {
-
+    
     @IBOutlet weak var taskNameLabel: UILabel!
-    @IBOutlet weak var countdonwLabel: UILabel!
+    @IBOutlet weak var countdownLabel: UILabel!
     @IBOutlet weak var progressSlider: UISlider!
+    
     
     private var taskName: String!
     private var countdownTime: Int! // 単位：1分
+    private var countdownTimer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialization()
     }
-
-    @IBAction func completeButtonTapped(_ sender: Any) {
+    
+    func initialization() {
+        taskNameLabel.text = taskName
+        updateCountdownTime()
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(CountdownViewController.timerUpdate), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerUpdate() {
+        countdownTime -= 1
+        updateCountdownTime()
+        if countdownTime == 0 {
+            timeup()
+        }
+    }
+    
+    func timeup() {
+        countdownTimer.invalidate()
+        // TODO:バイブレーション
+        
+    }
+    
+    func updateCountdownTime() {
+        let hour = Int(floor(Double(countdownTime) / 3600.0))
+        let minute = Int(floor(Double(countdownTime) / 60.0))
+        let second = Int(countdownTime % 60)
+        
+        var outputTime = ""
+        if (hour == 0) {
+            outputTime = String(format: "%02d:%02d", minute, second)
+        } else {
+            outputTime = String(format: "%02d:%02d:%02d", hour, minute, second)
+        }
+        countdownLabel.text = outputTime
+    }
+    
+    @IBAction func finishButtonTapped(_ sender: Any) {
         
     }
     
