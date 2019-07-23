15ebf20bb4d502a4d71d7f52100c7a3b6c4cc8de
Atsushi Otsubo <m@rirex.net>
2019-07-23 22:52:36 +0900

    [Add]TimerSetViewの実装

--- toDO/CountdownViewController.swift
+++ toDO/CountdownViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class CountdonwViewController: UIViewController {
+class CountdownViewController: UIViewController {
 
     @IBOutlet weak var taskNameLabel: UILabel!
     @IBOutlet weak var countdonwLabel: UILabel!
     @IBOutlet weak var progressSlider: UISlider!
+    
+    private var taskName: String!
+    private var countdownTime: Int! // 単位：1分
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,15 @@ class CountdonwViewController: UIViewController {
     }
 
     @IBAction func completeButtonTapped(_ sender: Any) {
+        
+    }
+    
+    func setTaskName(taskName: String) {
+        self.taskName = taskName
+    }
+    
+    func setCountdownTime(timeMinutes: Int) {
+        self.countdownTime = timeMinutes
     }
     
     
--- toDO/TimerSetViewController.swift
+++ toDO/TimerSetViewController.swift
@@ -8,20 +8,52 @@
 
 import UIKit
 
-class TimerSetViewController: UIViewController {
+class TimerSetViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var taskNameTextField: UITextField!
-    
     @IBOutlet weak var countdownDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        taskNameTextField.delegate = self
     }
     
     @IBAction func startButtonTapped(_ sender: Any) {
+        if taskNameTextField.text != "" {
+            showCountdownView()
+        } else {
+            showAlart(title: "タスク名を入力してください", message: "")
+        }
+    }
+    
+    func showCountdownView() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let countdownViewController = storyboard.instantiateViewController(withIdentifier: "CountdownView") as! CountdownViewController
         
+        let taskName = taskNameTextField.text!
+        let timeMinutes = Int(countdownDatePicker.countDownDuration)
+        countdownViewController.setTaskName(taskName: taskName)
+        countdownViewController.setCountdownTime(timeMinutes: timeMinutes)
+        // partial ページめくり的な，coverVertical 下から上がってくる，crossDissolve ふわっと入れ替わる，flipHorizontal 画面がクルッと180度回る
+        countdownViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve // アニメーション
+        self.present(countdownViewController, animated: true, completion: nil)
+    }
+    
+    func showAlart(title: String, message: String) {
+        let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle:  UIAlertController.Style.alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // To close keyboard by enter button
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
+    // To close keyboard by tap
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
