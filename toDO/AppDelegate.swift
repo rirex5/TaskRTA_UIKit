b2ac0e3dfdea63d4ee842f40ee336a2ca2712fce
Atsushi Otsubo <m@rirex.net>
2019-08-04 19:07:42 +0900

    [Add] TabBarの選択時の色を変更

--- toDO/AppDelegate.swift
+++ toDO/AppDelegate.swift
@@ -14,10 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var backgroundTaskID : UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0)
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor.darkGray
         return true
     }
 
